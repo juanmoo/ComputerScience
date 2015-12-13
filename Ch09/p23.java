ec0bc8dd892e494985f7355940e434770c7c62d3
juanmoo <juanmoo@me.com>
2015-12-13 20:41:45 +0000

    Added capability to add list to p23

--- Ch09/p23.java
+++ Ch09/p23.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class p23 {
     public static void main(String args[]) {
-        double[] hello = new double[]{1,2,3,4,5,6,7,8,9};
-        double num = 4;
-        
-        System.out.println(isMedian(hello, num));
+//        double[] hello = new double[]{1,2,3,4,5,6,7,8,9};
+  //      double num = 4;
+	Scanner kb = new Scanner(System.in);
+	System.out.print("Enter the number of elements in the list: ");
+	int length = kb.nextInt();
+
+	double[] list = new double[length];
+
+	for (int i = 0; i <= length-1; i++) {
+		System.out.print("Enter element number "+(i+1)+" :");
+		list[i] = kb.nextInt();
+		System.out.println();
+	}
+	System.out.print("Enter number: ");
+	int num = kb.nextInt();
+
+	String array = Arrays.toString(list);
+	System.out.println("The list is :"+array+".");        
+
+        System.out.println(isMedian(list, num));
     }
     
     public static boolean isMedian(double[] list, double median) {
